30f5cfc15ed691f498b4267a33ca0232686a4044
agarwalanushka <46869197+agarwalanushka@users.noreply.github.com>
2021-09-28 16:57:31 +0530

    Create sort.py
    Python program to sort a dictionary based on the values 
    Assignment on sep 28

---
+++ sort.py
@@ -0,0 +1,15 @@
+
+markdict = {"one":'one', "five": 'five', "four": 'four', "seven": 'seven', "two": 'two'}
+marklist = sorted(markdict.items())
+sortdict = dict(marklist)
+print(sortdict)
+
+
+
+
+
+
+
+
+
+ 
